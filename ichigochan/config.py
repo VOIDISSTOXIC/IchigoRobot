b774850ee783f1ef780adfeddbda921690854bc7
Lynncept77 <95460649+Lynncept77@users.noreply.github.com>
2022-02-18 14:19:52 +0530

    Create config.py

---
+++ ichigochan/config.py
@@ -0,0 +1,32 @@
+# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
+import json
+import os
+
+
+def get_user_list(config, key):
+    with open("{}/Ichigochan/{}".format(os.getcwd(), config), "r") as json_file:
+        return json.load(json_file)[key]
+
+
+# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
+class Config(object):
+    LOGGER = True
+    # REQUIRED
+    # Login to https://my.telegram.org and fill in these slots with the details given by it
+
+    API_ID = 123456  # integer value, dont use ""
+    API_HASH = "awoo"
+    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 5069705982  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Lynncept_iz_here"
+    # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
+    LOAD = []
+    NO_LOAD = ["rss", "cleaner", "connection", "math"]
+    
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True
